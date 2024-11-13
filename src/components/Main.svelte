cb77c0daa2101f7f3dc61886fd67c4c67a56edc5
SillySharp <rieszpeti@gmail.com>
2024-11-13 22:53:33 +0100

    add email and cv also some transition

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,14 +1,39 @@
 <script lang="ts">
+	import { fade } from "svelte/transition";
+
     interface MainButton {
       name: string;
       href: string;
     }
 
+    const email = import.meta.env.VITE_MY_EMAIL;
+    const cv = import.meta.env.VITE_CV_LINK
+
     const buttons: MainButton[] = [
-    { name: 'About', href: '#' },
-    { name: 'Email', href: '#' },
-    { name: 'CV', href: import.meta.env.VITE_CV_LINK }
+        { name: 'About', href: '/About' },
+        { name: 'Email', href: email },
+        { name: 'CV', href: cv }
     ];
+
+    let isShowEmailMessage = false;
+    let emailMessage = '';
+
+    const copyEmail = () => {
+        navigator.clipboard.writeText(email)
+            .then(() => {
+                emailMessage = "Email copied";
+                isShowEmailMessage = true;
+                setTimeout(() => isShowEmailMessage = false, 5000);
+            })
+            .catch(err => {
+                emailMessage = 'Oops, something went wrong while copying the email. Please try again later.';
+                isShowEmailMessage = true;
+                if (import.meta.env.MODE === 'development') {
+                    console.error('Failed to copy email: ', err);
+                }
+            });
+    };
+
 </script>
 
 <h1 class="text-2xl mt-4 max-sm:flex max-sm:flex-col max-sm:items-center max-sm:justify-center">
@@ -24,14 +49,29 @@
 
 <div class="flex flex-wrap gap-4 mt-4 max-sm:flex-col max-sm:items-center max-sm:space-y-2">
     {#each buttons as { name, href }}
-        <button class="rounded w-28 py-2 button-bg-color text-white hover:bg-gray-600 max-sm:w-40">
-            <a href={href}>
-                {name}
-            </a>
+        <button 
+            class="rounded w-28 py-2 button-bg-color text-white hover:bg-gray-600 max-sm:w-40"
+            on:click={() => { 
+                if (name === 'Email') {
+                    copyEmail();
+                } else {
+                    window.location.href = href;
+                }
+            }}
+        >
+            {name}
         </button>
     {/each}
 </div>
-  
+
+{#if isShowEmailMessage}
+  <div class="notification fixed top-0 right-0 m-4 bg-gray-800 text-white p-4 rounded shadow-md flex items-center z-50"
+    out:fade>
+    <p class="mr-4">{emailMessage}</p>
+    <button on:click={() => isShowEmailMessage = false} class="text-white text-xl">&times;</button>
+  </div>
+{/if}
+
 <style>
     .button-bg-color {
         background-color: #2b2b2b;
