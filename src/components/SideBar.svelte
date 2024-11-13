0666a50e6abe0b8d460616ffb6f4ca9d885e1609
SillySharp <rieszpeti@gmail.com>
2024-11-13 19:45:22 +0100

    remove comment

--- src/components/SideBar.svelte
+++ src/components/SideBar.svelte
@@ -48,7 +48,6 @@
           {#each sectionItems as tab}
             <li>
               <a href={tab.href} class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                <!-- Render the icon directly -->
                 <tab.icon class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
                 <span class="ms-3">{tab.name}</span>
               </a>
