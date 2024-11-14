cc87cf93451a365508edc6c03ebb5657f02145da
SillySharp <rieszpeti@gmail.com>
2024-11-15 00:17:39 +0100

    try newer recommended build

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
-const dev = process.argv.includes('dev')
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter(),
-    paths: {
-      base: dev ? '' : process.env.BASE_PATH,
-    }
-  },
-  preprocess: vitePreprocess()
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
+
 export default config;
