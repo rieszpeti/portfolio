2b145744419a4bdbaf4788e9629822b2d798d165
SillySharp <rieszpeti@gmail.com>
2024-11-14 16:34:05 +0100

    add about WIP

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,3 +1,15 @@
 <h2 id="about" class="text-2xl mt-8 max-sm:flex max-sm:flex-col max-sm:items-center max-sm:justify-center">
     About
 </h2>
+
+<p class="mt-6">
+    Hi, I'm Peter Riesz, a passionate backend software engineer with a unique blend of technical expertise and a musical background. 
+    With experience in backend development, DevOps, and classical music, I’m dedicated to creating scalable, reliable solutions. 
+    I enjoy working on complex projects that merge efficiency with innovation, exploring new tech, and refining my skills to deliver impactful applications.
+</p>
+
+<h3 class="text-lg mt-6">Background</h3>
+<p class="mt-4">
+    Since starting my career, I’ve specialized in backend solutions, focusing on building robust applications and optimizing legacy systems. 
+    My experience spans C#, PostgreSQL, Docker, DevOps, and CI/CD in GitLab, along with contributions to big data projects.
+</p>
