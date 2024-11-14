832b167615ec2315c6bed5adfcfe7f6d585c8cc0
SillySharp <rieszpeti@gmail.com>
2024-11-14 16:16:55 +0100

    separate file to smaller component

---
+++ src/components/IntroSection.svelte
@@ -0,0 +1,10 @@
+<h1 class="text-2xl mt-4 max-sm:flex max-sm:flex-col max-sm:items-center max-sm:justify-center">
+    Hey, I'm Peter,<br>
+    <span class="block mt-2">A Backend software engineer.</span>
+</h1>
+
+<span class="block mt-2 font-color max-sm:flex max-sm:flex-col max-sm:items-center max-sm:justify-center">
+    Experienced in building reliable, scalable systems and web applications. 
+    Focused on creating efficient solutions while exploring new technologies 
+    to improve workflows and processes.
+</span>
