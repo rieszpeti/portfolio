1897cd2cb79ccd12792dddbc9480a7581a5351f5
SillySharp <rieszpeti@gmail.com>
2024-11-16 21:29:57 +0100

    try another method for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,8 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+			fallback: 'app.html'
+		})
 	}
 };
 
