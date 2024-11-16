6ac596d96405bcc42c8612038dab2f188cdc70e5
SillySharp <rieszpeti@gmail.com>
2024-11-16 22:01:25 +0100

    try

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,10 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: '404.html'
+			fallback: '404.html',
+      pages: 'build',
+			assets: 'build',
+			precompress: false
 		})
 	}
 };
